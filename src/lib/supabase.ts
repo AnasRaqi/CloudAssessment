a5f39b5c54b19233272cf78d51262a099ba40d22
AnasRaqi <anas.azzouni@gmail.com>
2025-11-01 22:16:08 +0300

    2

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://dasayklxuitycwesuzmc.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhc2F5a2x4dWl0eWN3ZXN1em1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzOTg0MjUsImV4cCI6MjA3NTk3NDQyNX0.MpQkAN28Re4EYTr_1rnxwt86z81rvzS1rVYiKpHGKfw';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
